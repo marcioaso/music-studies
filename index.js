9b19820561605a279c9959e85459df5cdc5f8adf
Marcio Oliveira <Marcio Oliveira>
2019-09-09 00:07:34 -0300

    note calculation

--- index.js
+++ index.js
@@ -1,46 +1,116 @@
-const notesAbs = ["C","D","E","F","G","A","B"];
-const notesSemi = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 const tunning = ["E","A","D","B","E"];
+const notesAbs = ["C","D","E","F","G","A","B"];
+const notesBemol = ["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"];
+const notesSust = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+const formulas = {
+    scales:{
+        M:["t","t","st","t","t","st"], // major natural
+        m:["t","st","t","t","t","st"]  // minor natural
+    },
+    triad:["0","3","5"],
+};
 
-function scale(first,isMinor) {
-    const naturalScales = {M:[2,7],m:[1,4], };
-    let notes = notesSemi.slice();
-    let full = notes.slice().concat(notes), init = full.indexOf(first.replace("#",''));
-    let ret = [], count = 0;
-    let semitones = naturalScales[isMinor?"m":"M"];
-    for (var i = 0; i < 7; i++) {
-        ret.push(full[init+count]);
-        if(!semitones.includes(i)) count++;
-        count++;
-    }
-    return ret.concat(first).join(" ");
-}
-function tabs() {
-    let full = notesSemi.slice().concat(notesSemi).concat(notesSemi);
-
-    let arm = [], string = [];
-
-    for (var a = 0; a < tunning.length; a++) {
-        let start = full.indexOf(tunning[a]);
-        for( var s = 0; s < 12; s++) {
-            string.push(full[start+s]);
+var utils = {
+    sustAbs: function(bemolNote) {
+        if(bemolNote.match(/#/)) return bemolNote;
+        let bNotes = this.multiply(notesBemol,2);
+        let sNotes = this.multiply(notesSust,2);
+        let bIndex = bNotes.indexOf(bemolNote);
+        return sNotes[bIndex];
+    },
+    tailhead: function(arr,start) {
+        let index = arr.indexOf(start);
+        let ret = arr.slice();
+        let tail = ret.splice(index,arr.length)
+        return tail.concat(ret);
+    },
+    multiply: function(arr,number) {
+        let ret = arr.slice();
+        for(var i = 0; i < number; i++) {
+            ret = ret.concat(arr.slice());
         }
-        arm.push(string);
-        string = [];
+        return ret;
+    },
+    voice: function(note,variation,isTriad) {
+        let v = note.match(/[A-G](#|b)/);
+        let string;
+        if(v && isTriad) { // # e b
+            string = this.string(this.sustAbs(note),24);
+            return string[variation];
+        }
+
+        let th = this.multiply(this.tailhead(notesAbs,note),10);
+        let voice = parseInt(variation);
+        let transpiled = isNaN(voice);
+        let voiceAbs = !+variation?1:variation;
+        if(!transpiled) return th[voiceAbs-1];
+        
+        voice = + variation.replace(/\D/g,'');
+
+        string = this.string(note,24);
+        string = this.tailhead(string,note)
+        let sum = {"bb":-2,"b":-1,"#":+1}[variation.replace(/\d/g,'')];
+        let variatedNote = th[voice-1];
+        
+        variatedNote = string.indexOf(variatedNote)+sum;
+        return string[variatedNote];
+    },
+    string: function(init,size = 12) {
+        init = init.toUpperCase();
+        let output = [];
+        let index = notesSust.indexOf(init), count = 0;
+        while(output.length < size) {
+            if(!notesSust[index+count]) count = index = 0;
+            output.push(notesSust[index+count]);
+            count++;
+        }
+        return output;
     }
-    return arm;
 }
 
-const noteFormulas = {
-    T:[0,2,4],
-
+const services = {
+    scale: function(init = "c", scaleType = 0) {
+        const string = utils.string(init,14)
+        let ret = [], semitones = formulas[scaleType];
+        let i = 0, count = 0;
+        while(ret.length < 7) {
+            ret.push(string[i]);
+            i += semitones[count];
+            count++;
+        }
+        return ret.concat(init.toUpperCase()).join(" ");
+    }
 }
+
+const tabs = () => tunning.slice().map(init => utils.string(init));
 
 function note(str) {
-    let full = notesSemi.slice().concat(notesSemi).concat(notesSemi);
-    const input = str.replace(/[^0-9A-ZdimM+-]/g,'').split('');
-    console.log(input);
-}
+    let firstNote = str.match(/^([A-G])(b|#)?/)[0];
+    let triad = formulas.triad.map(variation => utils.voice(firstNote,variation,true));
+    if(str.length > 1) {
+        let noteArr = str
+            .replace(/\//g,'|')
+            .replace(/maj(\d+)/ig,'|#$1|')
+            .replace(/(\d+)\+/ig,'|#$1|')
+            .replace(/add(\d)/ig,'|$1|')
+            .replace(/(\d)\/(\d)/ig,'|$1|$2|')
+            .replace(/flat(\d)/ig,'|b$1|')
+            .replace(/^([A-G](#|b)?)m/,function(_,g) {
+                triad[1] = utils.voice(triad[0],'b3');
+                return g+"|";
+            })
+            .replace(/\//g,'|')
+            .replace(/[^b#](\d+)/g,'|$1|');
 
-//console.log(scale("C"))
-note("C")
+        noteArr = noteArr.replace(/(\|+)/g,'|').replace(/\|$/,'').split("|");
+        noteArr.shift(); // major note
+        triad = triad.concat(noteArr.map(variation => utils.voice(triad[0],variation)));
+    }
+    return triad;
+}
+console.log(note("C"))
+console.log(note("Cm"))
+console.log(note("C7"))
+console.log(note("Cm7"))
+console.log(note("Cm7+"))
+console.log(note("C7/9"))
